27d65c9ac32833eedc90542cfc0cb2fbd0cae69d
Jung Lee <lee.jung.s95@gmail.com>
2020-05-12 05:51:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,7 +49,7 @@ class Song
 
   def self.create_from_filename(filename)
     song = self.new
-    song.new_from_filename(filename)
+    self.new_from_filename(filename)
     song.save
     song
   end
