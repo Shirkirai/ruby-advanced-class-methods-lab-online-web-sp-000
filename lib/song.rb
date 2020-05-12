681b83afcc39cbf4a51dcd2f3584bf4734d27394
Jung Lee <lee.jung.s95@gmail.com>
2020-05-12 05:14:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name("#{name}")
+    if self.find_by_name(name)
       self.all
     elsif
       self.create_by_name(name)
