ff38e6fb1d0bf3bd936748d398b4b21ef7a12ca8
Jung Lee <lee.jung.s95@gmail.com>
2020-05-12 05:37:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,8 @@ class Song
   def self.new_from_filename(artist_song_name)
     s = self.new
     rows = artist_song_name.split("\n")
+    people = rows.collect do |row|
+      data = row.split(", ")
   end
 
 
